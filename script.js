b58efbf7ab266656c66e4acd050356045a3422de
xsalif <swlxzy@yaani.com>
2025-11-29 15:17:58 +0300

    İlk commit: Proje oluşturuldu

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
-function selamla() {
-    alert("Merhaba!");
+alert("Merhaba!");
+
+document.getElementById("btn").addEventListener("click", () => {
+    
+document.getElementById("sonra").textContent = "butona tıkladın.";
+});
+
+let sayi = 5;
+sayi = sayi + 10;
+
+const ad = "Eren";
+
+console.log("Toplam", sayi);
+console.log("İsim:", ad);
+
+if (5>3) {
+    console.log("5 3'ten büyük babacum");
 }
 
-const selamla2 = () => alert("Selam2!");
+let yas = 17;
+
+if (yas>=18) {
+    console.log("Girebilirsin")
+} else {
+    console.log("18 ol gel >.<");
+}
+
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+    alert("Butona tıkladın!");
+})
+
+const p =
+document.getElementById("sonuc");
+p.textContent = "Js ile yazıldı";
+
+document.getElementById("btn").addEventListener("click", () => { document.getElementById("sonra").textContent = "butona tıladın2";});
