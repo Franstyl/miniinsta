5c30e75df845f2440eb413d89eb1b2959b65706e
xsalif <swlxzy@yaani.com>
2025-12-01 10:35:41 +0300

    Js düzenlemesi: button Düzelttim.

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+const buton2 = document.getElementById("buton2");
 const button = document.getElementById("buton1");
 const p = document.getElementById("Deneme");
+const degis = document.getElementById("degis");
+const isim = prompt("Adını yaz tatlım 💖:");
 
-button.addEventListener('click', () => {
-    p.textContent("DEĞİŞTİM!");    
-})
+button.addEventListener("click", () => {
+    p.textContent = "DEĞİŞTİM!";    
+});
+
+buton2.addEventListener("click", () => {
+    degis.textContent = "BY SEFER BEN DEGİSTİM";
+    const yenip = document.createElement('p');
+    yenip.textContent = "Ben yeni eklendim!"; // önce yazıyı ekle
+    document.body.appendChild(yenip); // sonra sayfaya ekle
+});
+alert("Hoşgeldin " + isim + " 😻");
+
+const devam = confirm("Devam etmek istiyor musun? 🌸");
+if (devam) {
+    alert("Süper, devam ediyoruz!! 🎉");
+} else {
+    alert("Tamam, ara veriyoruz.");
+}
